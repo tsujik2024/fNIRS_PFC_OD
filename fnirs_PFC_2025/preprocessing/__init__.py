81293874815b13d33f23b59205c4976272adf11c
tsujik2024 <163925184+tsujik2024@users.noreply.github.com>
2026-09-16 16:48:43 -0700

    Update __init__.py

--- fnirs_PFC_2025/preprocessing/__init__.py
+++ fnirs_PFC_2025/preprocessing/__init__.py
@@ -1,17 +1,13 @@
-"""
-fNIRS preprocessing modules including filtering, artifact correction, and signal processing.
-"""
-
 from .average_channels import average_channels
 from .baseline_correction import baseline_subtraction
-from .fir_filter import fir_filter
+from .butterworth_filter import butterworth_bandpass
 from .short_channel_regression import scr_regression
 from .tddr import tddr
 
 __all__ = [
     'average_channels',
     'baseline_subtraction',
-    'fir_filter',
+    'butterworth_bandpass',
     'scr_regression',
     'tddr'
 ]
